6afa7be6b2f2d70a34cb28a5acf8fa1314a809e6
Fabiano Teodoro <faateodoro@gmail.com>
2022-05-06 19:54:17 -0300

    criando a estrutura inicial

--- src/lib.rs
+++ src/lib.rs
@@ -1,8 +1,34 @@
-#[cfg(test)]
-mod tests {
-    #[test]
-    fn it_works() {
-        let result = 2 + 2;
-        assert_eq!(result, 4);
+#![allow(dead_code, unused_variables)]
+
+struct Credentials {
+    username: String,
+    password: String
+}
+
+enum Status {
+    Connected,
+    Interrupted
+}
+
+fn connect_to_database() -> Status {
+    Status::Connected
+}
+
+fn login(creds: Credentials) {
+    // authenticate
+    get_user();
+}
+
+fn get_user() {
+    // get user from database
+}
+
+fn logout() {
+    // log user out
+}
+
+fn authenticate(creds: Credentials) {
+    if let Status::Connected = connect_to_database() {
+        login(creds);
     }
 }
